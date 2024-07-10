6442ef2e9bfcdc2557caa45e03efab245cd17f62
Ruben Martinez <dario564@hotmail.es>
2024-07-10 14:27:13 -0500

    Se modifico el archivo views.py de Proveedor

--- Config/app/views/Proveedor/views.py
+++ Config/app/views/Proveedor/views.py
@@ -1,35 +1,19 @@
-from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.utils.decorators import method_decorator
-from django.shortcuts import render
-from typing import Any
-from django.http import JsonResponse
 from django.urls import reverse_lazy
-from django.http.response import HttpResponse as HttpResponse
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 from app.models import Proveedor
 from app.forms import ProveedorForm
-
-@login_required
-def lista_proveedor(request):
-    context = {
-        'titulo': 'Listado de Proveedores',
-        'proveedores': Proveedor.objects.all(),
-        'crear_url': reverse_lazy('app:proveedor_crear')  # Asegúrate de tener esta línea
-    }
-    return render(request, 'Proveedor/listarP.html', context)
 
 class ProveedorListView(ListView):
     model = Proveedor
     template_name = 'Proveedor/listarP.html'
-    
+
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
-        
-    def post(self, request, *args, **kwargs):
-        data = {'Nombre': 'Lorena'}
-        return JsonResponse(data)
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = 'Listado de Proveedores'
@@ -55,7 +39,7 @@ class ProveedorUpdateView(UpdateView):
     form_class = ProveedorForm
     template_name = 'Proveedor/crearP.html'
     success_url = reverse_lazy('app:proveedor_listarP')
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['titulo'] = 'Actualizar Proveedor'
@@ -74,3 +58,12 @@ class ProveedorDeleteView(DeleteView):
         context['entidad'] = 'Proveedor'
         context['listar_url'] = reverse_lazy('app:proveedor_listarP')
         return context
+
+@login_required
+def lista_proveedor(request):
+    context = {
+        'titulo': 'Listado de Proveedores',
+        'proveedores': Proveedor.objects.all(),
+        'crear_url': reverse_lazy('app:proveedor_crear')
+    }
+    return render(request, 'Proveedor/listarP.html', context)
