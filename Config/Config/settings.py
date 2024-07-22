c1de0b23927395dc5b34712f7818bd452a9ef20d
LorenMS <loresserrano11@gmail.com>
2024-07-22 15:28:29 -0500

    Agregue Olvido contraseña
    se agrego el Botón de olvide Contraseña, a los templeates de login se agrefo una carpeta REGISTER la cual tiene los archivos de password

--- Config/Config/settings.py
+++ Config/Config/settings.py
@@ -104,3 +104,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = '/login/'
+
+# Olvido su contraseña
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ruben961103@gmail.com' 
+EMAIL_HOST_PASSWORD = 'paco tjrt dyyi irwq'
