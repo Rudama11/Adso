4aae59057f6a08b43d7642efd936296211f30f06
Rudama11 <dario564@hotmail.es>
2024-07-31 17:45:12 -0500

    Cambios

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\Aprendiz Sena\Documents\GitHub\Adso\.venv
+set VIRTUAL_ENV=c:\Users\juers\OneDrive\Documentos\GitHub\Adso\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
