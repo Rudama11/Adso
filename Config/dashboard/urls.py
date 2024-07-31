118336d02051928a4445f47e7e1a29f96b0d3e27
Ruben Martinez <165716669+Rudama11@users.noreply.github.com>
2024-07-31 15:05:35 -0500

    Borrado de la carpeta
    Se borra debido a un error al momento de cargar la carpeta backup.

--- Config/dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path 
-from dashboard.views import*
-
-urlpatterns = [
-    path('', dashView.as_view(), name='dashboard')
-]
--- Config/dashboard/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-class dashView(TemplateView):
-    template_name = 'dashboard.html'
