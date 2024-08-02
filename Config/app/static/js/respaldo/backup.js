3f28946f63c4d9aaa2eb4705e3595b29aa00deea
Rudama11 <dario564@hotmail.es>
2024-08-02 15:07:30 -0500

    Ajustes visuales de backup
    Se realizo un ajuste visual al funcionamiento de crear y restaurar el backup.

--- Config/app/static/js/respaldo/backup.js
+++ Config/app/static/js/respaldo/backup.js
@@ -1,9 +1,8 @@
+document.getElementById('select-file-button').addEventListener('click', function() {
+    document.getElementById('backup-file-input').click();
+});
 
-        document.getElementById('select-file-button').addEventListener('click', function() {
-            document.getElementById('backup-file-input').click();
-        });
-
-        document.getElementById('backup-file-input').addEventListener('change', function() {
-            var fileName = this.files[0].name;
-            document.getElementById('file-name').textContent = fileName;
-        });
+document.getElementById('backup-file-input').addEventListener('change', function() {
+    var fileName = this.files[0].name;
+    document.getElementById('file-name').textContent = fileName;
+});
--- Config/app/views/backup/views.py
+++ Config/app/views/backup/views.py
@@ -1,15 +1,62 @@
+# import os
+# import shutil
+# import datetime
+# from django.shortcuts import render, redirect
+# from django.http import HttpResponse
+# from django.conf import settings
+# from django.contrib import messages
+
+# def backup_view(request):
+#     # Agregar un mensaje de éxito si se envió un backup correctamente
+#     if 'backup_success' in request.GET:
+#         messages.success(request, "Copia de seguridad creada exitosamente.")
+#     return render(request, 'backup.html')
+
+# def backup_database(request):
+#     backup_dir = os.path.join(settings.BASE_DIR, 'backups')  # Ruta de la carpeta backups
+#     os.makedirs(backup_dir, exist_ok=True)  # Crear la carpeta si no existe
+
+#     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+#     backup_file = os.path.join(backup_dir, f'backup_{timestamp}.sqlite3')
+
+#     try:
+#         shutil.copy(os.path.join(settings.BASE_DIR, 'db.sqlite3'), backup_file)
+#         # Redirigir a la vista de backup y mostrar un mensaje de éxito
+#         return redirect('app:backup')  # Asegúrate de que el nombre sea correcto
+#     except Exception as e:
+#         return HttpResponse(f"Error al crear la copia de seguridad: {e}")
+
+# def restore_database(request):
+#     if request.method == 'POST' and request.FILES['backup_file']:
+#         backup_file = request.FILES['backup_file']
+
+#         # Definir la ruta de la base de datos actual
+#         db_file = os.path.join(settings.BASE_DIR, 'db.sqlite3')
+
+#         # Guardar la copia de seguridad cargada
+#         with open(db_file, 'wb+') as destination:
+#             for chunk in backup_file.chunks():
+#                 destination.write(chunk)
+
+#         return HttpResponse("Base de datos restaurada exitosamente.")
+#     else:
+#         return HttpResponse("Error al restaurar la base de datos.")
+
 import os
 import shutil
 import datetime
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.conf import settings
 from django.contrib import messages
+from django.urls import reverse
 
 def backup_view(request):
     # Agregar un mensaje de éxito si se envió un backup correctamente
     if 'backup_success' in request.GET:
         messages.success(request, "Copia de seguridad creada exitosamente.")
+    if 'restore_success' in request.GET:
+        messages.success(request, "Base de datos restaurada exitosamente.")
     return render(request, 'backup.html')
 
 def backup_database(request):
@@ -22,7 +69,7 @@ def backup_database(request):
     try:
         shutil.copy(os.path.join(settings.BASE_DIR, 'db.sqlite3'), backup_file)
         # Redirigir a la vista de backup y mostrar un mensaje de éxito
-        return redirect('app:backup')  # Asegúrate de que el nombre sea correcto
+        return HttpResponseRedirect(reverse('app:backup') + '?backup_success=True')
     except Exception as e:
         return HttpResponse(f"Error al crear la copia de seguridad: {e}")
 
@@ -38,6 +85,7 @@ def restore_database(request):
             for chunk in backup_file.chunks():
                 destination.write(chunk)
 
-        return HttpResponse("Base de datos restaurada exitosamente.")
+        # Redirigir a la vista de backup y mostrar un mensaje de éxito
+        return HttpResponseRedirect(reverse('app:backup') + '?restore_success=True')
     else:
         return HttpResponse("Error al restaurar la base de datos.")
