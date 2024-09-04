13f17c3e3c2735eb29a674ca8dfb8c43d3e6fd14
LorenMS <loresserrano11@gmail.com>
2024-09-04 17:09:53 -0500

    Ajustes
    Se modifico en los settings el correo de enviar contraseña, tambien la validacion del login y correciones en los html

--- Config/Config/settings.py
+++ Config/Config/settings.py
@@ -114,21 +114,21 @@ LOGIN_REDIRECT_URL = '/dashboard'
 LOGOUT_REDIRECT_URL = '/login/'
 
 # Olvido su contraseña
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com' 
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ruben961103@gmail.com' 
-EMAIL_HOST_PASSWORD = 'paco tjrt dyyi irwq'
-
-
-#inicio
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST = 'smtp.gmail.com' 
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'boyacaconaldex@gmail.com'
-# EMAIL_HOST_PASSWORD = 'ewfh mhlu txgs cnls'
+# EMAIL_HOST_USER = 'ruben961103@gmail.com' 
+# EMAIL_HOST_PASSWORD = 'paco tjrt dyyi irwq'
+
+
+#inicio,# Olvido su contraseña
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'boyacaconaldex@gmail.com'
+EMAIL_HOST_PASSWORD = 'ewfh mhlu txgs cnls'
  
  
 #Correo Electronico 
