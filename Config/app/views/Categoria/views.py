92e43b609b8220d1d6afd95892e32a814db8e9a9
Rudama11 <dario564@hotmail.es>
2024-09-17 17:56:49 -0500

    Ajuste 1.0 de modulo editacion de registro

--- Config/app/views/Categoria/views.py
+++ Config/app/views/Categoria/views.py
@@ -61,9 +61,10 @@ class CategoriaUpdateView(UpdateView):
     template_name = 'Categoria/editarC.html'
     success_url = reverse_lazy('app:categoria_listar')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['titulo'] = 'Actualizar Categoría'
-        context['entidad'] = 'Categoría'
-        context['listar_url'] = reverse_lazy('app:categoria_listar')
-        return context
+    def form_valid(self, form):
+        messages.success(self.request, 'La categoría ha sido actualizada exitosamente.')
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, 'Debe llenar todos los campos de registro correctamente.')
+        return super().form_invalid(form)
